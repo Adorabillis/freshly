331d4abc2ff777282202b49a983d038f93e3fe0f
Irina <sabaliauskieneirina@gmail.com>
2021-05-10 14:42:36 +0000

    gggg

--- controllers/TableauDeBordController.php
+++ controllers/TableauDeBordController.php
@@ -6,9 +6,17 @@ class TableauDeBordController {
 
 	public function display()
 	{
+		if(!isset($_SESSION['admin']))
+	{
+		    header('location:index.php?page=admin');
+			exit;
+	}
+	else 
+	{
 		//afficher le formulaire de connexion
             $template = 'views/tableauDeBord.phtml';
             include 'views/layout_front.phtml';
 	}
 
+}
 }
